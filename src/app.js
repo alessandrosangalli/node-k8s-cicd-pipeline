8284948fe5e8cd754b384c3bc630281857592dad
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2025-12-22 16:13:34 -0300

    .

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     logger.info('Root endpoint called');
-    res.json({ message: 'Hello from the Gold Standard Pipeline.', version: '1.0.0' });
+    res.json({ message: 'Hello from the Gold Standard Pipeline. Error simulation', version: '1.0.0' });
 });
 
 app.get('/health', (req, res) => {
