e22d305df90b330ba3fb7e5afaabe5e1d108736d
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2025-12-23 21:22:47 -0300

    feat: Implement Node.js application with Argo Rollouts for canary deployments and Prometheus metrics.

--- src/app.js
+++ src/app.js
@@ -31,11 +31,11 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    logger.info('Root endpoint called');
-    res.json({ message: 'Hello from the Gold Standard Pipeline. Success simulation 7', version: '1.0.0' });
+    // logger.info('Root endpoint called');
+    // res.json({ message: 'Hello from the Gold Standard Pipeline. Success simulation 7', version: '1.0.0' });
     // //teste
-    // logger.info('Root endpoint called - Simulating Failure');
-    // res.status(500).json({ error: 'Critical Business Logic Failure' });
+    logger.info('Root endpoint called - Simulating Failure');
+    res.status(500).json({ error: 'Critical Business Logic Failure' });
 });
 
 app.get('/health', (req, res) => {
