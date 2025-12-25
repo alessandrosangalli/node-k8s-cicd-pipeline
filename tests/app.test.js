13bd10920e18098186da9519e5cd5307ff988e17
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2025-12-24 22:36:33 -0300

    feat: Implement initial Express.js application with basic routes, Prometheus metrics, security middleware, and unit tests.

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,11 +2,11 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /', () => {
-    // it('should return a message property', async () => {
-    //     const res = await request(app).get('/');
-    //     // We only check for the property because the status might be 500 or 200 depending on tests
-    //     expect(res.body).toHaveProperty('message' || 'error');
-    // });
+    it('should return a message property', async () => {
+        const res = await request(app).get('/');
+        // We only check for the property because the status might be 500 or 200 depending on tests
+        expect(res.body).toHaveProperty('message' || 'error');
+    });
 });
 
 describe('GET /health', () => {
