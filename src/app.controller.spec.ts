1bafc10192f43f3dfea9bc174353d772a9ea6c45
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2025-12-26 18:57:57 -0300

    feat: Add E2E and AppController unit tests, configure Jest, and update package dependencies.

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('AppController', () => {
+    let appController: AppController;
+
+    beforeEach(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+        }).compile();
+
+        appController = app.get<AppController>(AppController);
+    });
+
+    describe('root', () => {
+        it('should return a greeting message', () => {
+            expect(appController.getHello()).toEqual({
+                message: 'Hello from the Gold Standard Pipeline. Success simulation 9',
+            });
+        });
+    });
+
+    describe('health', () => {
+        it('should return UP status', () => {
+            expect(appController.getHealth()).toEqual({
+                status: 'UP',
+                timestamp: expect.any(Date),
+            });
+        });
+    });
+});
